49b9c94b81542d9c87c8af895c0be46fced45a76
Routalanttu <pyry.takkunen@cs.tamk.fi>
2016-09-14 10:21:37 +0300

    Mikolle testiä

---
+++ Slingy Things/Assets/Scripts/Kokeilupaskaa/MoiMikkoScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoiMikkoScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Moi, Mikko!");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
