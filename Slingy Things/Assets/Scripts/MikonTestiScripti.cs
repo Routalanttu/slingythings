8400d984398b114121cbaea6e5bd95c3d242e009
Mikko McMenamin <oneofhaze@gmail.com>
2016-09-14 10:07:42 +0300

    eka commit

---
+++ Slingy Things/Assets/Scripts/MikonTestiScripti.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MikonTestiScripti : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("Moimoi"); 
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
