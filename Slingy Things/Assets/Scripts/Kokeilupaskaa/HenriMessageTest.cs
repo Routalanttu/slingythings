8608b7feb3f2c10d19e049899aebc8a527cbaa15
unknown <c5hriutt@PELIKONE8-PC.wpk.tpu.fi>
2016-09-14 10:44:14 +0300

    Henrin eka commit

---
+++ Slingy Things/Assets/Scripts/Kokeilupaskaa/HenriMessageTest.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HenriMessageTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log ("Hurr durr");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
