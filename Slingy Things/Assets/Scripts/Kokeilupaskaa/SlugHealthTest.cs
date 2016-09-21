a394a95f5860c2477ce3d4c40cc5b8d23e44ed34
unknown <c5hriutt@pelikone2-PC.wpk.tpu.fi>
2016-09-21 14:12:14 +0300

    Räjähdys logiikkaa ja HP bar aloitusta/testailua.

--- Slingy Things/Assets/Scripts/Kokeilupaskaa/SlugHealthTest.cs
+++ Slingy Things/Assets/Scripts/Kokeilupaskaa/SlugHealthTest.cs
@@ -1,21 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class SlugHealthTest : MonoBehaviour {
 
+    public Vector3 hpBar;
 
-    void OnGUI()
-    {
-        // Make a group on the center of the screen
-        GUI.BeginGroup(new Rect(Screen.width / 2 - 50, Screen.height / 2 - 50, 100, 100));
-        // All rectangles are now adjusted to the group. (0,0) is the topleft corner of the group.
-
-        // We'll make a box so you can see where the group is on-screen.
-        GUI.Box(new Rect(0, 0, 100, 100), "Group is here");
-        GUI.Button(new Rect(10, 40, 80, 30), "Click me");
-
-        // End the group we started above. This is very important to remember!
-        GUI.EndGroup();
+	// Use this for initialization
+	void Start () {
 
     }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localScale = hpBar;
+	}
 }
