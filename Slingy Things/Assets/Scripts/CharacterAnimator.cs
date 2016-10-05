8a153d25e11293fe23ad58157c024014292fadc5
Routalanttu <pyry.takkunen@cs.tamk.fi>
2016-10-05 16:02:54 +0300

    Character Animator 0.5; vielä pikkuvikoja kuten stretchtailin jääminen näkyviin.

---
+++ Slingy Things/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace SlingySlugs {
+	public class CharacterAnimator : MonoBehaviour {
+
+		[SerializeField] private SpriteRenderer _head;
+		[SerializeField] private SpriteRenderer _idleTail;
+		[SerializeField] private SpriteRenderer _stretchTail;
+		[SerializeField] private SpriteRenderer _counterPiece;
+		[SerializeField] private SpriteRenderer _flight;
+		[SerializeField] private Sprite _healthyIdle;
+		[SerializeField] private Sprite _damagedIdle;
+		[SerializeField] private Sprite _fuckedIdle;
+		[SerializeField] private Sprite _stretchedIdle;
+		[SerializeField] private Transform _counterPieceTransform;
+		[SerializeField] private Transform _stretchTailTransform;
+
+		public void SetIdleTailVisibility (bool state) {
+			_idleTail.enabled = state;
+		}
+
+		public void SetHeadVisibility (bool state) {
+			_head.enabled = state;
+		}
+
+		public void SetFlightVisibility (bool state) {
+			_flight.enabled = state;
+			_head.enabled = !state;
+			_idleTail.enabled = !state;
+		}
+
+		public void SetStretchMode (bool state) {
+			SetIdleTailVisibility (!state);
+			_stretchTail.enabled = state;
+			_counterPiece.enabled = state;
+		}
+
+		public void GetDamaged () {
+			_head.sprite = _damagedIdle;
+		}
+
+		public void GetFucked () {
+			_head.sprite = _fuckedIdle;
+		}
+
+		public void RotateTail (Quaternion q) {
+			_counterPieceTransform.rotation = q;
+			//_stretchTailTransorm.rotation = q;
+		}
+
+		public void ScaleTail (float stretch) {
+			_stretchTailTransform.localScale = new Vector3 (stretch / 1.25f, 1f, 1f);
+		}
+
+		public void StretchFlip (bool state) {
+			_head.flipX = state;
+			_idleTail.flipX = state;
+		}
+
+		public void FlightFlip (bool state) {
+			_flight.flipY = state;
+		}
+
+
+
+
+
+
+	}
+}
