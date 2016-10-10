08c533fc11f24e24e8d7d2d6a7fcd477a1480cbb
Routalanttu <pyry.takkunen@cs.tamk.fi>
2016-10-10 15:47:08 +0300

    Explosion animation now works with horrible purkka. Will make prettier next. Also, colliders were altered to a lower detail level.

--- Slingy Things/Assets/_Scripts/Gameplay/Explosion.cs
+++ Slingy Things/Assets/_Scripts/Gameplay/Explosion.cs
@@ -12,11 +12,18 @@ namespace SlingySlugs {
 		private bool _fire;
 		private CharacterInfo _slug; 
 
+		private Transform xplo;
+		private Animator niggers;
+
 		// Use this for initialization
 		void Start () {
 
 			_gcTransform = GetComponent<Transform>(); 
 			_slug = GetComponent<CharacterInfo> (); 
+
+			xplo = GameObject.FindGameObjectWithTag ("Explosion").transform;
+			xplo.position = new Vector2 (9000f, 9000f);
+			niggers = xplo.gameObject.GetComponent<Animator> ();
 
 		}
 
@@ -35,6 +42,9 @@ namespace SlingySlugs {
 		}
 
 		void Fire(){
+
+			xplo.position = transform.position;
+			niggers.SetBool ("Blown", true);
 
 			Vector2 explosionPos = _gcTransform.position; 
 			ParticleSystem explosion; 
@@ -63,10 +73,13 @@ namespace SlingySlugs {
 			GameManager.Instance.NextPlayerMove();
 
 
+
 		}
 
 		public void ArmSlug(){
 			Armed = true;
+			niggers.SetBool ("Blown", false);
+			xplo.position = new Vector2 (9000f, 9000f);
 		}
 
 		public void NextPlayerMove(){
