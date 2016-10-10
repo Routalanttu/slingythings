024b0e6b00b15df68b0f7ce8e48a6c7020b43c64
Henri Riutta <henri.riutta@cs.tamk.fi>
2016-10-10 23:22:56 +0300

    Uusien menujen kanssa kikkailua

--- Slingy Things/Assets/_Scripts/Henrinskriptit/Menu2Test.cs
+++ Slingy Things/Assets/_Scripts/Henrinskriptit/Menu2Test.cs
@@ -5,15 +5,51 @@ public class Menu2Test : MonoBehaviour {
 
     public GameObject menuObjects;
     public GameObject numberOfTeamsObjects;
+    public Transform centerScreen;
+    public Transform offScreenRight;
+    public Transform offScreenLeft;
+    public float speed = 5.0f;
+    private float startTime;
+    private float journeyLength;
+    private bool playPressCheck = false;
+    private bool creditsPressCheck = false;
 
     public void Start() {
-        numberOfTeamsObjects.transform.Translate(new Vector2 (200, 0));
+        numberOfTeamsObjects.transform.position = offScreenLeft.transform.position;
+        menuObjects.transform.position = centerScreen.transform.position;
+        startTime = Time.time;
+        journeyLength = Vector3.Distance(centerScreen.position, offScreenRight.position);
+    }
+
+    public void Update() {
+        float distCovered = (Time.time - startTime) * speed;
+        float fracJourney = distCovered / journeyLength;
+
+        if (playPressCheck) {
+            menuObjects.transform.position = Vector3.Lerp(centerScreen.position, offScreenRight.position, fracJourney);
+            numberOfTeamsObjects.transform.position = Vector3.Lerp(offScreenLeft.position, centerScreen.position, fracJourney);
+
+            if (menuObjects.transform.position == offScreenRight.transform.position) {
+                playPressCheck = false;
+            }
+        }
+        
+        if(creditsPressCheck) {
+            menuObjects.transform.position = Vector3.Lerp(offScreenRight.position, centerScreen.position, fracJourney);
+            numberOfTeamsObjects.transform.position = Vector3.Lerp(centerScreen.position, offScreenLeft.position, fracJourney);
+
+            if (numberOfTeamsObjects.transform.position == offScreenLeft.transform.position) {
+                creditsPressCheck = false;
+            }
+        }
     }
 
     public void OnPlay() {
-        menuObjects.transform.Translate(new Vector2 (200, 0));
-        numberOfTeamsObjects.transform.Translate(new Vector2 (0, 0));
+        playPressCheck = true;
     }
 
-
+    public void OnCreditsReturn() {
+        creditsPressCheck = true;
+    }
+    
 }
