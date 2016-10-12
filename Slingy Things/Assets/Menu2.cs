e28bd66950442f13dabc33484cb951ecc2624518
unknown <c5hriutt@pelikone2-PC.wpk.tpu.fi>
2016-10-12 15:15:52 +0300

    Menu2.0 tekemistä animaatioilla

---
+++ Slingy Things/Assets/Menu2.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu2 : MonoBehaviour {
+
+    public GameObject menuObjects;
+    public GameObject numberOfTeamsObjects;
+    public Transform centerScreen;
+    public Transform offScreenRight;
+    public Transform offScreenLeft;
+    public Animator mainMenuObjects;
+    public Animator mainMenuCredits;
+    private bool playPressCheck = false;
+    private bool creditsPressCheck = false;
+
+
+    // Use this for initialization
+    void Start () {
+        numberOfTeamsObjects.transform.position = offScreenLeft.transform.position;
+        menuObjects.transform.position = centerScreen.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPlay() {
+        mainMenuObjects.SetTrigger("Menu2MainMenuObjectsMoveAwayFromScreen");
+        mainMenuCredits.SetTrigger("Menu2CreditsMoveToScreen");
+    }
+
+    public void OnCreditsReturn() {
+        mainMenuObjects.SetTrigger("Menu2MainMenuObjectsMoveAwayFromScreen");
+        mainMenuCredits.SetTrigger("Menu2CreditsMoveToScreen");
+    }
+
+}
