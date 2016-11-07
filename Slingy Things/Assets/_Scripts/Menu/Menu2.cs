dd9226b6dab69612c91f3437ce6716a11ae39dfb
Mikko McMenamin <oneofhaze@gmail.com>
2016-11-07 14:41:38 +0200

    Projektin siistimistä ja menun päivitystä

---
+++ Slingy Things/Assets/_Scripts/Menu/Menu2.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Menu2 : MonoBehaviour {
+
+    public GameObject menuObjects;
+	public GameObject teamsAndAnimals;
+	public GameObject selectLevel;
+    
+    public Animator mainMenuObjects;
+    public Animator teamsAndAnimalsObjects;
+	public Animator selectLevelObjects; 
+
+	//public GameObject numberOfTeamsObjects;
+
+    // Use this for initialization
+    void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnPlay() {
+        mainMenuObjects.SetTrigger("MainMenuMove");
+        teamsAndAnimalsObjects.SetTrigger("TeamsAndAnimalsMove");
+    }
+
+    public void OnTeamsAndAnimalsReturn() {
+        mainMenuObjects.SetTrigger("MainMenuMove");
+        teamsAndAnimalsObjects.SetTrigger("TeamsAndAnimalsMoveBack");
+    }
+
+	public void OnToLevelSelect(){
+		teamsAndAnimalsObjects.SetTrigger ("TeamsAndAnimalsMove"); 
+		selectLevelObjects.SetTrigger ("SelectLevelMove"); 
+
+	}
+
+	public void OnLevelSelectReturn(){
+		teamsAndAnimalsObjects.SetTrigger ("TeamsAndAnimalsMove"); 
+		selectLevelObjects.SetTrigger ("SelectLevelMove"); 
+	}
+
+    public void OnCredits() {
+
+    }
+
+    public void OnCreditsReturn() {
+
+    }
+
+}
