43388c5ca533b5596ca7f14a8369786cb90d2ab5
Routalanttu <pyry.takkunen@cs.tamk.fi>
2016-11-15 19:17:16 +0200

    Effect Destroyer script added; explosions and other poof get destroyed after playing their animations once. Octopi now have higher sorting layers so they don't clip with slug parts (temporary solution; see Trello card).

---
+++ Slingy Things/Assets/_Scripts/Gameplay/EffectDestroyer.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EffectDestroyer : MonoBehaviour {
+	void Start () {
+		Destroy (transform.gameObject, this.GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).length); 
+	}
+}
--- Slingy Things/Assets/_Scripts/Gameplay/Explosion.cs
+++ Slingy Things/Assets/_Scripts/Gameplay/Explosion.cs
@@ -4,7 +4,7 @@ using System.Collections;
 namespace SlingySlugs {
 	public class Explosion : MonoBehaviour {
 
-		[SerializeField] private float radius = 5.0F;
+		[SerializeField] private float _radius = 5.0F;
 		[SerializeField] private float _explosionForce = 10F;
 		[SerializeField] private int _explosionDamageMultiplier = 2; 
 		[SerializeField] private ParticleSystem _explosionPrefab; 
@@ -28,14 +28,14 @@ namespace SlingySlugs {
 			Instantiate(_explosionAnimation, _gcTransform.position, Quaternion.identity);
 			SoundController.Instance.PlaySoundByIndex (0); 
 
-			Collider2D[] colliders = Physics2D.OverlapCircleAll(xploPos, radius);
+			Collider2D[] colliders = Physics2D.OverlapCircleAll(xploPos, _radius);
 			foreach (Collider2D hit in colliders) {
 				Rigidbody2D rb = hit.GetComponent<Rigidbody2D>();
 
 				Vector2 hitPosition = hit.GetComponent<Transform>().position;
 				Vector2 explosionDelta = hitPosition- xploPos; //Get vector between explosion and hit 
 				Vector2 explosionDir = Vector3.Normalize (explosionDelta); //normalize the vector to get direction only 
-				float deltaDistance = radius - explosionDelta.magnitude; //get the effective blast magnitude
+				float deltaDistance = _radius - explosionDelta.magnitude; //get the effective blast magnitude
 				int explosionDamage = (int)deltaDistance * _explosionDamageMultiplier; 
 
 				if (rb != null && hit.gameObject.tag == "Slug" && rb!= this.gameObject.GetComponent<Rigidbody2D>()){
