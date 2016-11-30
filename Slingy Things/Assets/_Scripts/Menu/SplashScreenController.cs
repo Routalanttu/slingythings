121b798bb0d481269d18013fb7624e7229877efb
Henri Riutta <henri.riutta@cs.tamk.fi>
2016-11-30 15:23:21 +0200

    Splash screenin koskettamalla skippaus ja muuta hiomista

--- Slingy Things/Assets/_Scripts/Menu/SplashScreenController.cs
+++ Slingy Things/Assets/_Scripts/Menu/SplashScreenController.cs
@@ -6,18 +6,35 @@ public class SplashScreenController : MonoBehaviour {
 
     public Animator _splashScreenAnim;
 
-	// Use this for initialization
-	void Start () {
-        _splashScreenAnim.SetTrigger("Animate");
+    private AsyncOperation async;
 
+
+	void Start () {
+        ClickAsync();
+        _splashScreenAnim.SetTrigger("Animate");
     }
 
     void animationEnded () {
-        SceneManager.LoadScene("Menu");
+        ActivateMenu();
     }
 	
-	// Update is called once per frame
 	void Update () {
-        //animationEnded();
-	}
+        if (Input.GetMouseButtonDown(0))
+            ActivateMenu();
+    }
+
+    public void ClickAsync() {
+        StartCoroutine(LoadMenuWithAnimation());
+    }
+
+    IEnumerator LoadMenuWithAnimation () {
+        async = SceneManager.LoadSceneAsync("Menu");
+        async.allowSceneActivation = false;
+        yield return async;
+    }
+
+    public void ActivateMenu() {
+        async.allowSceneActivation = true;
+    }
+
 }
