fe553a1e6795d473ffe464160e5b693ec4816c76
Henri Riutta <henri.riutta@cs.tamk.fi>
2016-12-03 01:40:37 +0200

    Oli jotain ongelmia ollut aikasemmin, ei ollut muutokset mennyt perille jostain syystä

--- Slingy Things/Assets/_Scripts/Menu/SplashScreenController.cs
+++ Slingy Things/Assets/_Scripts/Menu/SplashScreenController.cs
@@ -6,18 +6,36 @@ public class SplashScreenController : MonoBehaviour {
 
     public Animator _splashScreenAnim;
 
-	// Use this for initialization
-	void Start () {
+    private AsyncOperation async;
+
+
+    void Start() {
+        ClickAsync();
         _splashScreenAnim.SetTrigger("Animate");
-
     }
 
-    void animationEnded () {
-        SceneManager.LoadScene("Menu");
+    void animationEnded() {
+        ActivateMenu();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        //animationEnded();
-	}
+
+    void Update() {
+        if (Input.GetMouseButtonDown(0))
+            ActivateMenu();
+    }
+
+    public void ClickAsync() {
+        StartCoroutine(LoadMenuWithAnimation());
+    }
+
+    IEnumerator LoadMenuWithAnimation() {
+        async = SceneManager.LoadSceneAsync("Menu");
+        async.allowSceneActivation = false;
+        yield return async;
+    }
+
+    public void ActivateMenu() {
+        async.allowSceneActivation = true;
+    }
+
 }
+
