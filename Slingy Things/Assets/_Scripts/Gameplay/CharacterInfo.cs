6936f72168773162e101dd96b0771495b98aa5ae
unknown <c5ptakku@PELIKONE8-PC.wpk.tpu.fi>
2017-01-08 13:04:54 +0200

    New death messages implemented.

--- Slingy Things/Assets/_Scripts/Gameplay/CharacterInfo.cs
+++ Slingy Things/Assets/_Scripts/Gameplay/CharacterInfo.cs
@@ -213,44 +213,9 @@ namespace SlingySlugs {
 		}
 
 		public void Die(){
-			TextMesh tmpDamageText = (TextMesh)Instantiate(_damageText, _myTransform.position, _myTransform.rotation);
+			TextMesh tmpDamageText = (TextMesh)Instantiate(_damageText, _myTransform.position, _myTransform.rotation); 
 
-			string deathString; 
-
-			switch ((int)Random.Range (0, 8)) {
-			case 0:
-				deathString = "R.I.P 1"; 
-				break;
-			case 1:
-				deathString = "R.I.P 2"; 
-				break;
-			case 2:
-				deathString = "R.I.P 3"; 
-				break;
-			case 3:
-				deathString = "R.I.P 4"; 
-				break;
-			case 4:
-				deathString = "R.I.P 5"; 
-				break;
-			case 5:
-				deathString = "R.I.P 6"; 
-				break;
-			case 6:
-				deathString = "R.I.P 7"; 
-				break;
-			case 7:
-				deathString = "R.I.P 8 "; 
-				break;
-			case 8:
-				deathString = "R.I.P 9"; 
-				break;
-			default: 
-				deathString = "R.I.P default"; 
-				break; 
-			}
-
-			tmpDamageText.text = deathString + "\n" + _characterName; 
+			tmpDamageText.text = RandomizeDeathText ();
 			tmpDamageText.color = Color.gray;
             SoundController.Instance.PlaySoundByIndex((int)Random.Range(10, 14)); 
 			_dead = true;
@@ -289,6 +254,228 @@ namespace SlingySlugs {
 
 		public void SetDeath () {
 			_dead = true;
+		}
+
+		private string RandomizeDeathText() {
+			string deathMessage;
+
+			switch ((int)Random.Range (0, 69)) {
+			case 0:
+				deathMessage = _characterName + " assumed" + "\n" + "room temperature";
+				break;
+			case 1:
+				deathMessage = _characterName + " is" + "\n" + "counting worms";
+				break;
+			case 2:
+				deathMessage = _characterName + " bit" + "\n" + "the big one";
+				break;
+			case 3:
+				deathMessage = _characterName + " made" + "\n" + "like a tree"; 
+				break;
+			case 4:
+				deathMessage = _characterName + " is" + "\n" + "wrapped in plastic"; 
+				break;
+			case 5:
+				deathMessage = _characterName + " did" + "\n" + "not make it"; 
+				break;
+			case 6:
+				deathMessage = _characterName + " dropped" + "\n" + "like a fly"; 
+				break;
+			case 7:
+				deathMessage = _characterName + " was" + "\n" + "exterminated"; 
+				break;
+			case 8:
+				deathMessage = _characterName + " is" + "\n" + "food for worms"; 
+				break;
+			case 9:
+				deathMessage = _characterName + "\n" + "went bung"; 
+				break;
+			case 10:
+				deathMessage = _characterName + " went the" + "\n" + "way of all flesh"; 
+				break;
+			case 11:
+				deathMessage = _characterName + " went" + "\n" + "out with a bang"; 
+				break;
+			case 12:
+				deathMessage = _characterName + " cashed" + "\n" + "in the chips"; 
+				break;
+			case 13:
+				deathMessage = _characterName + " took" + "\n" + "a last bow"; 
+				break;
+			case 14:
+				deathMessage = _characterName + " answered" + "\n" + "the final summons"; 
+				break;
+			case 15:
+				deathMessage = _characterName + " rode" + "\n" + "the pale horse"; 
+				break;
+			case 16:
+				deathMessage = _characterName + "\n" + "hopped the twig"; 
+				break;
+			case 17:
+				deathMessage = _characterName + " joined" + "\n" + "the choir invisible"; 
+				break;
+			case 18:
+				deathMessage = _characterName + " joined" + "\n" + "the great majority"; 
+				break;
+			case 19:
+				deathMessage = _characterName + "\n" + "kicked the bucket"; 
+				break;
+			case 20:
+				deathMessage = _characterName + " was sent" + "\n" + "to the farm upstate"; 
+				break;
+			case 21:
+				deathMessage = _characterName + " made the" + "\n" + "ultimate sacrifice"; 
+				break;
+			case 22:
+				deathMessage = _characterName + " was" + "\n" + "murder-death-killed"; 
+				break;
+			case 23:
+				deathMessage = _characterName + " is not" + "\n" + "with us anymore"; 
+				break;
+			case 24:
+				deathMessage = _characterName + " is" + "\n" + "off on a boat"; 
+				break;
+			case 25:
+				deathMessage = _characterName + " is" + "\n" + "off the hooks"; 
+				break;
+			case 26:
+				deathMessage = _characterName + "'s" + "\n" + "hour came"; 
+				break;
+			case 27:
+				deathMessage = _characterName + "'s" + "\n" + "number was up"; 
+				break;
+			case 28:
+				deathMessage = _characterName + "\n" + "pegged out"; 
+				break;
+			case 29:
+				deathMessage = _characterName + " was" + "\n" + "put to sleep"; 
+				break;
+			case 30:
+				deathMessage = _characterName + " shuffled" + "\n" + "off this mortal coil"; 
+				break;
+			case 31:
+				deathMessage = _characterName + "\n" + "snuffed it"; 
+				break;
+			case 32:
+				deathMessage = _characterName + " took" + "\n" + "a dirt nap"; 
+				break;
+			case 33:
+				deathMessage = _characterName + "\n" + "is no more"; 
+				break;
+			case 34:
+				deathMessage = _characterName + " has" + "\n" + "ceased to be"; 
+				break;
+			case 35:
+				deathMessage = _characterName + " is" + "\n" + "bereft of life"; 
+				break;
+			case 36:
+				deathMessage = _characterName + " bought" + "\n" + "a one-way ticket"; 
+				break;
+			case 37:
+				deathMessage = _characterName + "\n" + "checked out"; 
+				break;
+			case 38:
+				deathMessage = _characterName + "\n" + "climbed the stairs"; 
+				break;
+			case 39:
+				deathMessage = _characterName + "\n" + "is defunct"; 
+				break;
+			case 40:
+				deathMessage = _characterName + "\n" + "expired"; 
+				break;
+			case 41:
+				deathMessage = _characterName + "\n" + "flatlined"; 
+				break;
+			case 42:
+				deathMessage = _characterName + " launched" + "\n" + "into eternity"; 
+				break;
+			case 43:
+				deathMessage = _characterName + " has" + "\n" + "left the building"; 
+				break;
+			case 44:
+				deathMessage = _characterName + "\n" + "was liquidated"; 
+				break;
+			case 45:
+				deathMessage = _characterName + " met" + "\n" + "an untimely end"; 
+				break;
+			case 46:
+				deathMessage = _characterName + " is" + "\n" + "out of business"; 
+				break;
+			case 47:
+				deathMessage = _characterName + " rang" + "\n" + "down the curtain"; 
+				break;
+			case 48:
+				deathMessage = _characterName + " took a" + "\n" + "permanent vacation"; 
+				break;
+			case 49:
+				deathMessage = _characterName + " has" + "\n" + "walked the plank"; 
+				break;
+			case 50:
+				deathMessage = _characterName + " was" + "\n" + "called home"; 
+				break;
+			case 51:
+				deathMessage = _characterName + " became" + "\n" + "a root inspector"; 
+				break;
+			case 52:
+				deathMessage = _characterName + " bought" + "\n" + "a pine condo"; 
+				break;
+			case 53:
+				deathMessage = _characterName + "\n" + "crossed the bar"; 
+				break;
+			case 54:
+				deathMessage = _characterName + " is" + "\n" + "living-impaired"; 
+				break;
+			case 55:
+				deathMessage = _characterName + " paid" + "\n" + "Charon's fare"; 
+				break;
+			case 56:
+				deathMessage = _characterName + " paid a" + "\n" + "debt to nature"; 
+				break;
+			case 57:
+				deathMessage = _characterName + " is" + "\n" + "out of print"; 
+				break;
+			case 58:
+				deathMessage = _characterName + "\n" + "popped off"; 
+				break;
+			case 59:
+				deathMessage = _characterName + "\n" + "was put down"; 
+				break;
+			case 60:
+				deathMessage = _characterName + " reached" + "\n" + "the finish line"; 
+				break;
+			case 61:
+				deathMessage = _characterName + " returned" + "\n" + "to the ground"; 
+				break;
+			case 62:
+				deathMessage = _characterName + " returned" + "\n" + "to the source"; 
+				break;
+			case 63:
+				deathMessage = _characterName + " has" + "\n" + "sprouted wings"; 
+				break;
+			case 64:
+				deathMessage = _characterName + "\n" + "succumbed"; 
+				break;
+			case 65:
+				deathMessage = _characterName + " was" + "\n" + "all she wrote"; 
+				break;
+			case 66:
+				deathMessage = _characterName + "\n" + "took a harp"; 
+				break;
+			case 67:
+				deathMessage = _characterName + "\n" + "faded away"; 
+				break;
+			case 68:
+				deathMessage = _characterName + " is" + "\n" + "at rest now"; 
+				break;
+			case 69:
+				deathMessage = _characterName + " made" + "\n" + "like a tree"; 
+				break;
+			default: 
+				deathMessage = _characterName + "\n" + "passed away";  
+				break; 
+			}
+
+			return deathMessage;
 		}
 	}
 }
